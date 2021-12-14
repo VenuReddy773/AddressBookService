717930a45173cf96b933ef5626ec840e1e94616f
“VenuReddy773” <venu706@gmail.com>
2021-12-14 20:39:01 +0530

    [VenuGopalReddy] Added: Created AddressBook Table

--- AddressBook/AddressBookService.sql
+++ AddressBook/AddressBookService.sql
@@ -2,3 +2,14 @@
 create database AddressBookService
 use AddressBookService
 
+--UC2-Create Table
+create table AddressBook
+(	FirstName varchar(100),
+	LastName varchar(100),
+	Address varchar(100),
+	City varchar(50),
+	State varchar(50),
+	Zip int,
+	PhoneNumber varchar(20),
+	Email varchar(50)
+)
